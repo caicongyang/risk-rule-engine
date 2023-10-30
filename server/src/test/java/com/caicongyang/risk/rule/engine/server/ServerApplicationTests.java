4f876c21e01fb92febb001e5ac5ea7d3a4f71c18
caicongyang <caicongyang@gmail.com>
2023-10-30 15:25:21 +0800

    init

---
+++ server/src/test/java/com/caicongyang/risk/rule/engine/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caicongyang.risk.rule.engine.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
